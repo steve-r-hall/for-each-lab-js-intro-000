f7b73cf4ccd391d3d33688de336f6654ea187368
Stephen Robert hall <steve.r.hall@outlook.com>
2019-08-24 11:21:30 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -3,6 +3,16 @@ function iterativeLog(array) {
 }
 
 function logentry(index,entry,array) {
-  console.log("${index}: ${element}");
+  console.log(`${index}: ${entry}`);
   return
+}
+
+function iterate(callback) {
+  var temp = ["A", "B", "C", "D", "E"]
+  temp.forEach(callback);
+  return temp
+}
+
+function doToArray(array, callback) {
+  array.forEach(callback)
 }
